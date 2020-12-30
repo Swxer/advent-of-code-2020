19d98b9121158bb802e38064782572115effe121
Swxer <swxergamer64@gmail.com>
2020-12-30 14:38:14 +1100

    finished part 2 of day1

--- day1/day1.py
+++ day1/day1.py
@@ -10,3 +10,10 @@ for x in l:
         if x + y == 2020:
             # if the pair is found, multiply and show result
             print(x*y)
+
+# for part 2
+for x in l:
+    for y in l:
+        for z in l:
+            if x + y + z == 2020:
+                print(x*y*z)
